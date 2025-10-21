e5b9a9180efdccda0b100b5eaf03e1c36ec52a9a
qalibismayilli <qalibismayilli8@gmail.com>
2025-10-21 22:35:24 +0400

    some changes

---
+++ email-notification-microservice/src/main/java/org/example/emailnotificationmicroservice/handler/ProductCreatedEventHandler.java
@@ -0,0 +1,18 @@
+package org.example.emailnotificationmicroservice.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = "product-created-events-topic")
+public class ProductCreatedEventHandler {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+//    @KafkaHandler
+//    public void handle(ProductCreatedEvent event) {
+//    }
+}
