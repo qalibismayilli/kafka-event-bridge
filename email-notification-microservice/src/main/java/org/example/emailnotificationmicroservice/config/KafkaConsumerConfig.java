dcca4158a11089242ad91cc9165805f38823a332
qalibismayilli <qalibismayilli8@gmail.com>
2025-10-23 14:43:54 +0400

    some changes

---
+++ email-notification-microservice/src/main/java/org/example/emailnotificationmicroservice/config/KafkaConsumerConfig.java
@@ -0,0 +1,42 @@
+package org.example.emailnotificationmicroservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConsumerConfig {
+
+    private final Environment env;
+
+    public Map<String,Object> consumerConfigs() {
+        Map<String,Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,env.getProperty("spring.kafka.consumer.bootstrap-servers"));
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, env.getProperty("spring.kafka.consumer.group-id"));
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, env.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
+        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, env.getProperty("spring.kafka.consumer.properties.spring.json.value.default.type"));
+        config.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, env.getProperty("spring.kafka.consumer.properties.spring.json.use.type.headers"));
+
+        return config;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+
+}
