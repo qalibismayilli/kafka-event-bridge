a6ad75cdad44ad8b267becf8374a482209bbb3ce
qalibismayilli <qalibismayilli8@gmail.com>
2025-10-24 01:29:16 +0400

    some changes

--- email-notification-microservice/src/main/java/org/example/emailnotificationmicroservice/config/KafkaConsumerConfig.java
+++ email-notification-microservice/src/main/java/org/example/emailnotificationmicroservice/config/KafkaConsumerConfig.java
@@ -1,12 +1,14 @@
 package org.example.emailnotificationmicroservice.config;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.example.emailnotificationmicroservice.handler.ProductCreatedEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -16,27 +18,36 @@ import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableKafka
 public class KafkaConsumerConfig {
 
     private final Environment env;
 
-    public Map<String,Object> consumerConfigs() {
-        Map<String,Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,env.getProperty("spring.kafka.consumer.bootstrap-servers"));
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.consumer.bootstrap-servers"));
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, env.getProperty("spring.kafka.consumer.group-id"));
         config.put(JsonDeserializer.TRUSTED_PACKAGES, env.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
-        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, env.getProperty("spring.kafka.consumer.properties.spring.json.value.default.type"));
+        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, ProductCreatedEvent.class);
         config.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, env.getProperty("spring.kafka.consumer.properties.spring.json.use.type.headers"));
 
         return config;
     }
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
+    public ConsumerFactory<String, ProductCreatedEvent> consumerFactory() {
         return new DefaultKafkaConsumerFactory<>(consumerConfigs());
     }
 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ProductCreatedEvent> kafkaListenerContainerFactory(
+            ConsumerFactory<String, ProductCreatedEvent> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, ProductCreatedEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
 
 }
--- product-microservice/src/main/java/org/example/productmicroservice/config/KafkaConfig.java
+++ product-microservice/src/main/java/org/example/productmicroservice/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package org.example.productmicroservice.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.example.productmicroservice.service.ProductCreatedEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,8 @@ import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,11 +23,11 @@ public class KafkaConfig {
     @Value("${spring.kafka.producer.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${spring.kafka.producer.key-serializer}")
-    private String keySerializer;
-
-    @Value("${spring.kafka.producer.value-serializer}")
-    private String valueSerializer;
+//    @Value("${spring.kafka.producer.key-serializer}")
+//    private String keySerializer;
+//
+//    @Value("${spring.kafka.producer.value-serializer}")
+//    private String valueSerializer;
 
     @Value("${spring.kafka.producer.acks}")
     private String acks;
@@ -48,8 +51,8 @@ public class KafkaConfig {
     public Map<String,Object> producerConfigs(){
         Map<String,Object> config =new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         config.put(ProducerConfig.ACKS_CONFIG, acks);
         config.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, deliveryTimeout);
         config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
